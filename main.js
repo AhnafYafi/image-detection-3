859a9fe1b88de630943375a4ae2249ff4a88efd6
AhnafYafi <79776773+AhnafYafi@users.noreply.github.com>
2021-11-25 17:16:00 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function people()
+{
+    window.location="index_1.html";
+}
+
+function computers()
+{
+    window.location="index_2.html";
+}
+
+function car()
+{
+    window.location="index_3.html";
+}
+
+function dogcat()
+{
+    window.location="index_4.html";
+}
